54cd3fd61c346af7dce02727e65026d47e165495
Maciej aXenDev Balcerzak <ithereplay@gmail.com>
2021-03-02 10:58:53 +0100

    Add animation fade in widget

--- dev/js/front/aXenServerList.js
+++ dev/js/front/aXenServerList.js
@@ -1,12 +1,24 @@
 const COOKIE_NAME_TOGGLE = 'aXenServerList_widget_toggle';
 
-document.querySelector('.aXenServerList_toggle').addEventListener('click', () => {
+document.querySelector('.aXenServerList_toggle').addEventListener('click', e => {
+  e.preventDefault();
   const serverList = document.querySelector('.aXenServerList');
   serverList.classList.toggle('aXenServerList_hide');
 
   if (serverList.classList.contains('aXenServerList_hide')) {
     ips.utils.cookie.set(COOKIE_NAME_TOGGLE, 1, true);
   } else {
+    const serverListContain = document.querySelector('.aXenServerList_ul');
+    serverListContain.classList.add('ipsAnim');
+    serverListContain.classList.add('ipsAnim_fade');
+    serverListContain.classList.add('ipsAnim_in');
+
+    setTimeout(() => {
+      serverListContain.classList.remove('ipsAnim');
+      serverListContain.classList.remove('ipsAnim_fade');
+      serverListContain.classList.remove('ipsAnim_in');
+    }, 450);
+
     ips.utils.cookie.unset(COOKIE_NAME_TOGGLE);
   }
 });
